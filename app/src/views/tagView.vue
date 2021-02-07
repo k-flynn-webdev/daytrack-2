e6501c10ceca1a61a939a9e8debefc5f92b4c430
kevin flynn <flynny85@gmail.com>
2021-02-07 14:41:55 +0000

    add basic tag page

--- app/src/views/tagView.vue
+++ app/src/views/tagView.vue
@@ -1,13 +1,21 @@
 <template>
 
   <div class="column is-11">
-<!--    <tag-detail />-->
+
+    <h2 class="title">Tag Detail</h2>
+
+    <div v-for="prop in tagProperties"
+         :key="prop"
+         class="tag-detail">
+      <label class="label is-inline-block mr-1">{{ prop }}</label>
+      <span class="ml-1 detail">{{ detail[prop] }}</span>
+    </div>
+
   </div>
 
 </template>
 
 <script>
-// import TagDetail from "@/components/tagDetail";
 import { TAG, USER } from '@/constants'
 import HttpService from "@/services/HttpService";
 
@@ -20,7 +28,15 @@ export default {
     }
   },
 
+  computed: {
+    tagProperties () {
+      if (!this.detail) return []
+      return Object.keys(this.detail)
+    }
+  },
+
   props: {
+    /** Tag ID as <String> */
     tag: {
       type: String,
       default: ''
