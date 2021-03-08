b0ca222cafed424e604477bcec9ec1d7929ef36a
kevin flynn <flynny85@gmail.com>
2021-03-08 23:08:30 +0000

    adding row class

--- app/src/components/trackCreate.vue
+++ app/src/components/trackCreate.vue
@@ -39,14 +39,16 @@
 
         </div>
 
-        <button class="button-add bubble has-fill-white pl-2 pr-1"
+        <button class="button-add bubble has-fill-white icon ml-1"
                 :class="{ 'is-loading': loading }"
                 :disabled="loading || !isValid"
                 type="submit">
-          <svg viewBox="0 0 512 512" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/"
-               style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
-            <path d="M428.725,256l-345.45,-234.449l0,468.898l345.45,-234.449Z" />
-          </svg>
+          <div class="icon">
+            <svg viewBox="0 0 512 512" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/"
+                 style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+              <path d="M428.725,256l-345.45,-234.449l0,468.898l345.45,-234.449Z" />
+            </svg>
+          </div>
         </button>
 
       </div>
