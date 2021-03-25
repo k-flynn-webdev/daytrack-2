1b606cb278af23d46823a28068c6deacfc4e9d56
kevin flynn <flynny85@gmail.com>
2021-03-25 22:32:04 +0000

    updated shell scripts

---
+++ updateCron.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### A LAZY DELAYED CI/CD APPROACH
+### A SIMPLE
+###           GIT CHECK
+###           TIME DELAY CHECK
+###           GIT PULL BASH SCRIPT
+### USE TO KEEP A BRANCH UPTO DATE WITH ITS REMOTE COUNTERPART
+
+DELAY=3600
+GIT_BRANCH="main"
+GIT_URL=`git remote get-url origin`
+
+git fetch
+
+GIT_LOCAL=`git show --no-notes --format=format:"%H" $GIT_BRANCH | head -n 1`
+GIT_REMOTE=`git show --no-notes --format=format:"%H" origin/$GIT_BRANCH | head -n 1`
+
+TIME_CURRENT=`date "+%s"`
+TIME_LAST_COMMIT=`git show -s --format=%ct $GIT_REMOTE`
+TIME_DIFF="$(($TIME_CURRENT - $TIME_LAST_COMMIT))"
+
+if [ $GIT_REMOTE != $GIT_LOCAL ] && [ "$TIME_DIFF" -gt "$DELAY" ]; then
+  echo "[ $GIT_URL :: $GIT_BRANCH ] Updated"
+  git pull --no-edit
+fi
