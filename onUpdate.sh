aeed03b97b6159137668c1d0c3f9cbc1f7ab0659
kevin flynn <flynny85@gmail.com>
2021-03-25 19:15:57 +0000

    updating for ci-cd dev

---
+++ onUpdate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## onUpdate ##
+## Build All Files ##
+
+## VARS
+NODE_ENV=production
+source .env
+
+## PATHS
+dir_project=$PWD;
+dir_project_api=$dir_project/api;
+dir_project_app=$dir_project/app;
+
+## APP
+cd $dir_project_app
+npm run build
+
+## API
+source `which virtualenvwrapper.sh`
+workon $APP_ENVIROMENT
+
+## ASSETS
+cd $dir_project_api
+python3 manage.py collectstatic --noinput
+cp $dir_project_app/dist/index.html $dir_project/static/index.html
