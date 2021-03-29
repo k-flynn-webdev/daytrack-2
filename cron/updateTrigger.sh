129eaca12d2c8536ffd530b3503d79fbc1699719
kevin flynn <flynny85@gmail.com>
2021-03-29 22:19:00 +0100

    updated cron

--- cron/updateTrigger.sh
+++ cron/updateTrigger.sh
@@ -5,22 +5,23 @@
 
 ## PATHS
 dir_project=$PWD;
-dir_project_api=$dir_project/api;
-dir_project_app=$dir_project/app;
+dir_project_api=$dir_project/../api;
+dir_project_app=$dir_project/../app;
+dir_project_static=$dir_project/../static;
 
 ## VARS
 NODE_ENV=production
-source .env
+source ../.env
 
 ## APP
 cd $dir_project_app
-npm run build
+$NPM run build
 
 ## API
-source `which virtualenvwrapper.sh`
+source $VENV
 workon $APP_ENVIROMENT
 
 ## ASSETS
 cd $dir_project_api
 python3 manage.py collectstatic --noinput
-cp $dir_project_app/dist/index.html $dir_project/static/index.html
+cp $dir_project_app/dist/index.html $dir_project_static/index.html
