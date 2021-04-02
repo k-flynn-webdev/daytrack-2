7def64bd36c47c6fcff42cf584856daafa495302
kevin flynn <flynny85@gmail.com>
2021-04-02 13:22:35 +0100

    new idea

--- production/start.sh
+++ production/start.sh
@@ -7,8 +7,9 @@ dir_project=$dir_project_production/..;
 ## VARS
 source $dir_project/.env
 
-# Add Build to PM2 processes
-pm2 start ../builder/npm --name "$WEB_NAME-builder" -- start
-
 # Add App to PM2 processes
 pm2 start ./run_api_production.sh --name "$WEB_NAME-app"
+
+# Add Build to PM2 processes
+cd ../builder;
+pm2 start npm --name "$WEB_NAME-builder" -- start
