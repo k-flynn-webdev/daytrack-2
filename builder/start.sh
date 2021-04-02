12c9743267a2df87a1a176e02a84688b2406fbbb
kevin flynn <flynny85@gmail.com>
2021-04-02 12:10:09 +0100

    add pm2 start file

---
+++ builder/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## PATHS
+dir_project_build=$(dirname $0);
+dir_project=$dir_project_build/..;
+
+## VARS
+source $dir_project/.env
+
+# Add Build Watcher to PM2 processes
+pm2 start npm --name "$WEB_NAME" -- start
