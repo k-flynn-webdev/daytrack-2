081c2b61b5d26fba5d3a74acf4eae2b8ec093eac
kevin flynn <flynny85@gmail.com>
2021-04-02 13:03:27 +0100

    trigger rebuild

---
+++ production/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## PATHS
+dir_project_production=$(dirname $0);
+dir_project=dir_project_production/..;
+
+## VARS
+source $dir_project/.env
+
+# Add Build Watcher to PM2 processes
+pm2 start npm --name "$WEB_NAME-app" -- start
